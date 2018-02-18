a71af3a60b777b35df826e04191581d42fba89fb
lucasmaryn <lucasmaryn@interia.pl>
2018-02-18 17:46:56 +0100

    processor added, spring implementation - modul spring-hw

---
+++ camel_spring-hw/src/main/java/javainuse/processor/MyProcessor.java
@@ -0,0 +1,11 @@
+package javainuse.processor;
+
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class MyProcessor implements Processor{
+    public void process(Exchange exchange) throws Exception {
+        System.out.println("Processor here, hello user.");
+    }
+}
